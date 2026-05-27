4939cf371951381c70b9a2014076a5936e713385
Adil9298 <adilkakkoo@gmail.com>
2026-05-27 13:26:35 +0530

    tenth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,8 @@
-import 'package:adil_portfolio/features/home/screens/home_screen.dart';
 import 'package:adil_portfolio/features/splash/screens/splash_screen.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: FirebaseOptions(
-  //       apiKey: "AIzaSyBziBgIh7W-KA2vT4hEnIs-qNTBKtDOvbk",
-  //       authDomain: "keygallery-34dd1.firebaseapp.com",
-  //       projectId: "keygallery-34dd1",
-  //       storageBucket: "keygallery-34dd1.appspot.com",
-  //       messagingSenderId: "116706487441",
-  //       appId: "1:116706487441:web:949cc5166bb25fa1092330",
-  //       measurementId: "G-TXNB1QVY5F"
-  //   ),
-  // );
   runApp(const MyApp());
 }
 
